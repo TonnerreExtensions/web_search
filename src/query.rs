96930d815291df6fb8629460ce9f4172e2746dc9
Yaxin Cheng <yaxin.cheng@icloud.com>
2020-05-30 16:08:10 -0400

    Removed manual split for header and body

--- src/query.rs
+++ src/query.rs
@@ -4,7 +4,7 @@ use crate::service::Service;
 use crate::suggestion_proc;
 
 pub fn query(request: &str, output: &str, identifier: &str, config: Config) {
-    let (request, initial_service) = process(request, &config);
+    let initial_service = build_service(env!("DEFAULT_URL"), env!("PROVIDER_NAME"), "...");
     if let Ok(initial_response) =
         Response::new(identifier, vec![initial_service]).serialize_to_json()
     {
@@ -17,23 +17,6 @@ pub fn query(request: &str, output: &str, identifier: &str, config: Config) {
     if let Ok(suggestion_response) = Response::new(identifier, suggestions).serialize_to_json() {
         std::fs::write(output, suggestion_response).expect("OUTPUT closed");
     }
-}
-
-fn process<'a>(request: &'a str, config: &Config) -> (&'a str, Service) {
-    let split_index = match request.find(" ") {
-        Some(index) => index,
-        None => {
-            return (
-                "",
-                build_service(env!("DEFAULT_URL"), env!("PROVIDER_NAME"), "..."),
-            )
-        }
-    };
-    let request = &request[split_index + 1..];
-    (
-        request,
-        build_service(&config.search_url(request), request, request),
-    )
 }
 
 pub fn build_service<S: Into<String>, T: Into<String>, R: Into<String>>(
